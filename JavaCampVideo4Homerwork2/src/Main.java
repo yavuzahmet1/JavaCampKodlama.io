711314c279964669047d9ff3f91ede7575e99940
Coders club <1yavuzahmet@gmail.com>
2022-08-18 12:41:42 +0300

    video 4 ödev 2 ödev taslak olarak tamamlanmıştır

--- JavaCampVideo4Homerwork2/src/Main.java
+++ JavaCampVideo4Homerwork2/src/Main.java
@@ -1,7 +1,28 @@
+import java.time.LocalDate;
+
+import concretes.CustomerManager;
+import concretes.GameProductManager;
+import entities.Campaign;
+import entities.Customer;
+import entities.GameProduct;
+
 public class Main {
 
 	public static void main(String[] args) {
+		LocalDate localDate = LocalDate.of(2021, 02, 03);
+		Customer customer = new Customer("Ahmet", "YAVUZ", "12345678909", 1986);
+		Customer customer2 = new Customer("Ahmet", "Kazan", "12345677909", 1987);
 
+		GameProduct gameProduct = new GameProduct("Mario", 1200.00, localDate);
+
+		Campaign campaign = new Campaign("Muhtesem Cuma Kampanyasý", 1000.00);
+
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.add(customer);
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager2.add(customer2);
+		GameProductManager gameProductManager = new GameProductManager();
+		gameProductManager.sellGameProduct(gameProduct, customer, campaign);
 	}
 
 }
