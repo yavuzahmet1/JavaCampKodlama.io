ad1cfa0b8a9e48c5e5599b5e90f2aa06ebbdf466
Coders club <1yavuzahmet@gmail.com>
2022-08-21 22:45:50 +0300

    business package created ProductDao (inteface class) extends JpaRepository

---
+++ northwind/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,15 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.northwind.entities.concretes.Product;
+
+public interface ProductDao extends JpaRepository<Product, Integer> {
+	// interface interfaceyi extends eder bu yüzden JpaRepository'i extends ediyoruz
+	// JpaRepository verdiğim veri tipi için Entity anatasyonuyla süslenmiş nesne
+	// için
+	// kısacası product için primaryKey anahtarınıda ver ki ben sorguları ona göre
+	// hazırlayalım
+	// primaryKey'im de integer olduğundan nesne Product,primaryKey Integer
+	// belirtilir.
+}
