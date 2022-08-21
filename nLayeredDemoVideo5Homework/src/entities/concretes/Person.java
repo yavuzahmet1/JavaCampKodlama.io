e87d6a53dca48b6128ed5c7c8e35173e645c4116
Coders club <1yavuzahmet@gmail.com>
2022-08-21 09:14:12 +0300

    Person class created

---
+++ nLayeredDemoVideo5Homework/src/entities/concretes/Person.java
@@ -0,0 +1,44 @@
+package entities.concretes;
+
+public class Person {
+	private String name;
+	private String surname;
+	private String email;
+	// private String password;
+
+	public Person() {
+
+	}
+
+	public Person(String name, String surname, String email) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
