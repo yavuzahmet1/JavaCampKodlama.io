198cccc3f7e3887ae574ad4704871a284c096e6c
Coders club <1yavuzahmet@gmail.com>
2022-08-21 22:30:26 +0300

    entities annotation completed

--- northwind/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,11 +1,32 @@
 package kodlamaio.northwind.entities.concretes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+	@Id
+	@GeneratedValue
+	@Column(name = "product_id")
 	private int id;
+
+	@Column(name = "category_id")
 	private int categoryId;
+
+	@Column(name = "product_name")
 	private String productName;
+
+	@Column(name = "unit_price")
 	private double unitPrice;
+
+	@Column(name = "units_in_stock")
 	private short unitsInStock;
+
+	@Column(name = "quantity_per_unit")
 	private String quantitiyPerUnit;
 
 	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
