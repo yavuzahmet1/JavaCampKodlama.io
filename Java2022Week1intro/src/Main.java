cad1785d86838ec38becc9e8d3f9dffbfb4c998c
Coders club <1yavuzahmet@gmail.com>
2022-09-14 23:00:47 +0300

    start

--- Java2022Week1intro/src/Main.java
+++ Java2022Week1intro/src/Main.java
@@ -2,7 +2,19 @@
 public class Main {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello word...");
+		String ortaMetin = "ilginizi çekebilir";
+		String altMetin = "Vade süresi";
+
+		System.out.println(ortaMetin);
+
+		int vade = (int) 1.2;
+		System.out.println(vade);
+
+		double dolarDun = 18.14;
+
+		boolean dolarDustuMu = false;
 
 	}
 
