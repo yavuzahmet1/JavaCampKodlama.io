eaef554fcf1260fe2a12f4fd6dfdbb657226a31c
Coders club <1yavuzahmet@gmail.com>
2022-12-06 21:24:46 +0300

    Homework-hrms project created

---
+++ week6/homework/hrms-demo/hrms-demo/src/test/java/com/hrms/demo/HrmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
