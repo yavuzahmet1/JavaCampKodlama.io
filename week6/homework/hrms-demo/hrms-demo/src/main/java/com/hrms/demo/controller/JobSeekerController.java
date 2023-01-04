4bb122e26f2ca185fd8d44efa677646926a73d5c
Coders club <1yavuzahmet@gmail.com>
2023-01-04 04:34:44 +0300

    Hrms controller created

---
+++ week6/homework/hrms-demo/hrms-demo/src/main/java/com/hrms/demo/controller/JobSeekerController.java
@@ -0,0 +1,9 @@
+package com.hrms.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jobseekers")
+public class JobSeekerController {
+}
