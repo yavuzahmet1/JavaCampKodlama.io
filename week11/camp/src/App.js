5b8ab5db8cac19819218abbf0a9818081bd28f7f
Coders club <1yavuzahmet@gmail.com>
2023-03-03 23:00:49 +0300

    react published

--- week11/camp/src/App.js
+++ week11/camp/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navi />
-      <Container className='main'>
+      <Container className="main">
         <Dashboard />
       </Container>
     </div>
